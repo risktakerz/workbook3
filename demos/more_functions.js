440847f442278cb6d3cab1788ebfb1c0f1532abc
risktakerz <paintkash@proton.me>
2024-10-15 14:53:11 -0400

    removed compact folders

--- demos/more_functions.js
+++ demos/more_functions.js
@@ -9,6 +9,7 @@ function displayMailingLabel(name, address, city, state, zip) {
 }
 
 displayMailingLabel("Kevin", "23 Hars ave", "BROOKLYN", "NY", 11022);
+console.log("-------------");
 displayMailingLabel("Gon", "23 Dead Continent ave", "LA", "California", 11022);
 
 function addNumbers(num1, num2) {
@@ -18,11 +19,17 @@ function addNumbers(num1, num2) {
 addNumbers(3, 5);
 addNumbers(7, 8);
 function displayReceipt(totalDue, amountPaid) {
+  if (totalDue < amountPaid) {
+    console.log("Error now");
+    return;
+  }
   console.log(`Total Due: ${totalDue}`);
+  console.log("-------------");
   console.log(`Amount Paid: ${amountPaid}`);
 }
-
-displayReceipt(300, 39);
-displayReceipt(200, 49);
-displayReceipt(23000, 5600);
+console.log("-------------");
+displayReceipt(20, 33333339);
+// displayReceipt(300, 39);
+// displayReceipt(200, 49);
+// displayReceipt(23000, 5600);
 console.log("-------------");
