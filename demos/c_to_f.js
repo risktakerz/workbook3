492dfd698e5ba22bea8815145890f0d165973072
risktakerz <paintkash@proton.me>
2024-10-16 15:47:17 -0400

    f to c and c to f calculators

---
+++ demos/c_to_f.js
@@ -0,0 +1,12 @@
+function convertCtoF(Celsius) {
+  Fahrenheit = (Celsius * 9) / 5 + 32;
+
+  return Fahrenheit;
+}
+
+convertCtoF(100);
+convertCtoF(45);
+convertCtoF(19);
+convertCtoF(0);
+convertCtoF(-7);
+convertCtoF(-40);
---
+++ demos/f_to_c.js
@@ -0,0 +1,14 @@
+function convertFtoC(Fahrenheit) {
+  let Celsius = (Fahrenheit - 32) / (9 / 5);
+  return Celsius;
+}
+
+convertFtoC(413.6);
+convertFtoC(194);
+convertFtoC(161.6);
+convertFtoC(89.6);
+convertFtoC(32);
+convertFtoC(-43.6);
+
+// let currentTemp = 92;
+// let celsiusTemp = convertFtoC(currentTemp);
