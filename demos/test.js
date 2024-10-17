3ff175730e282eddabf90500165ace8fad5764b7
risktakerz <paintkash@proton.me>
2024-10-16 22:13:36 -0400

    added files

---
+++ demos/test.js
@@ -0,0 +1,15 @@
+let amount = 20;
+let message1 = "The amount I owe " + amount + " dollars";
+console.log(message1);
+
+let message2 = `The amount you owe is ${amount}`;
+
+let snackFood = "Pop Tarts";
+let time0fDay = "evening";
+
+let message301dStyle =
+  "A healthy snack to have is " + snackFood + " every " + time0fDay + "!";
+let message3 = "A healthy snack to have is ${snackFood} every ${time0fDay}!";
+console.log(message301dStyle);
+
+console.log(message3);
